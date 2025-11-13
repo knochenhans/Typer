504560f37fa403faf33edee42dc956f9c619e647
Andre Jonas <nipsky@googlemail.com>
2025-11-13 16:16:48 +0100

    Split code into core and node

---
+++ addons/Typer/TyperCore.cs
@@ -0,0 +1,209 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Godot;
+
+#nullable enable
+
+public partial class TyperCore : GodotObject
+{
+	public enum StateEnum
+	{
+		Started,
+		Typing,
+		Pause,
+		Finished
+	}
+
+	public TyperResource Resource { get; private set; }
+	public TextureRect Target { get; private set; }
+	public Action? PlayTypingSound { get; private set; }
+
+	// Publicly readable typing state used by the TextureRect (TyperNode) for drawing
+	public string[] Lines { get; private set; } = [];
+	public string? CurrentLine { get; private set; }
+	public int CurrentLastLineIdx { get; private set; }
+	public int CurrentLastCharIdx { get; private set; }
+	public float[] LinesWidth { get; private set; } = [];
+	public float Height { get; private set; }
+	public int CurrentFinalCaretBlinkTimes { get; private set; }
+	public int CurrentFinalCaretBlinkTime { get; private set; }
+
+	// state manager
+	SimpleStateManager<StateEnum> StateManager;
+
+	// pause tags
+	Dictionary<int, List<(int Position, int Value)>> Pauses;
+
+	// events
+	public event Action? Updated;
+	public event Action? Finished;
+
+	public TyperCore(TyperResource resource, TextureRect target, Action? playTypingSound = null)
+	{
+		Resource = resource;
+		Target = target;
+		PlayTypingSound = playTypingSound;
+		StateManager = new SimpleStateManager<StateEnum>(StateEnum.Started);
+		Pauses = [];
+	}
+
+	public void Init(string text = "")
+	{
+		Pauses.Clear();
+		CurrentFinalCaretBlinkTime = 0;
+		CurrentFinalCaretBlinkTimes = 0;
+		CurrentLine = "";
+		CurrentLastCharIdx = 0;
+		CurrentLastLineIdx = 0;
+		Height = 0;
+
+		if (string.IsNullOrEmpty(text))
+			text = Resource.Text;
+
+		Lines = text.Split(new[] { System.Environment.NewLine }, StringSplitOptions.None);
+		LinesWidth = new float[Lines.Length];
+
+		for (int i = 0; i < Lines.Length; i++)
+		{
+			var size = Resource.Font.GetStringSize(Lines[i], fontSize: Resource.FontSize);
+			LinesWidth[i] = size.X;
+			Height += size.Y;
+
+			if (i < Lines.Length - 1)
+				Height += Resource.LineSpacing;
+
+			var pauses = ExtractPauses(ref Lines[i]);
+			if (pauses.Count > 0)
+				Pauses.Add(i, pauses);
+		}
+
+		Updated?.Invoke();
+	}
+
+	public async Task Start()
+	{
+		await Task.Delay((int)(Resource.StartDelay * 1000));
+		await SwitchState(StateEnum.Typing);
+	}
+
+	private async Task TypeLoop()
+	{
+		while (true)
+		{
+			if (CurrentLastLineIdx >= Lines.Length)
+			{
+				CurrentFinalCaretBlinkTimes = Resource.FinalCaretBlinkTimes;
+				await SwitchState(StateEnum.Pause);
+				break;
+			}
+
+			if (StateManager.CurrentState != StateEnum.Typing)
+				break;
+
+			CurrentLine = Lines[CurrentLastLineIdx];
+
+			if (CurrentLastCharIdx < CurrentLine.Length)
+			{
+				if (Pauses.TryGetValue(CurrentLastLineIdx, out var pausePositions))
+				{
+					foreach (var (position, value) in pausePositions.ToList())
+					{
+						if (CurrentLastCharIdx == position)
+						{
+							CurrentFinalCaretBlinkTimes = value;
+							// remove this pause and go to pause state
+							pausePositions.Remove((position, value));
+							await SwitchState(StateEnum.Pause);
+							return;
+						}
+					}
+				}
+
+				CurrentLastCharIdx++;
+				Updated?.Invoke();
+				PlayTypingSound?.Invoke();
+			}
+			else
+			{
+				CurrentLastLineIdx++;
+				CurrentLastCharIdx = 0;
+			}
+
+			await Task.Delay((int)(Resource.TypingSpeed * 1000));
+		}
+	}
+
+	static List<(int Position, int Value)> ExtractPauses(ref string input)
+	{
+		var tags = new List<(int Position, int Value)>();
+
+		string pattern = @"(?<!\\)\[([^\]]+)\]";
+
+		while (true)
+		{
+			Match match = Regex.Match(input, pattern);
+			if (!match.Success)
+				break;
+
+			input = input.Remove(match.Index, match.Length);
+			if (int.TryParse(match.Groups[1].Value, out int v))
+				tags.Add((match.Index, v));
+		}
+
+		return [.. tags.OrderBy(tag => tag.Value)];
+	}
+
+	public StateEnum CurrentState => StateManager.CurrentState;
+
+	private async Task SwitchState(StateEnum newState)
+	{
+		StateManager.CurrentState = newState;
+
+		switch (newState)
+		{
+			case StateEnum.Typing:
+				CurrentFinalCaretBlinkTime = 0;
+				await TypeLoop();
+				break;
+			case StateEnum.Pause:
+				while (CurrentFinalCaretBlinkTime < (CurrentFinalCaretBlinkTimes * 2) - 1)
+				{
+					CurrentFinalCaretBlinkTime++;
+					Updated?.Invoke();
+					await Task.Delay((int)(Resource.CaretBlinkTime * 1000));
+				}
+				if (CurrentLastLineIdx == Lines.Length)
+					await SwitchState(StateEnum.Finished);
+				else
+					await SwitchState(StateEnum.Typing);
+				break;
+			case StateEnum.Finished:
+				if (Resource.PreFadeoutTime > 0)
+					await Task.Delay((int)(Resource.PreFadeoutTime * 1000));
+
+				// fade the target TextureRect using the helper
+				await FadeHelper.TweenFadeModulate(Target, FadeHelper.FadeDirectionEnum.Out, Resource.FadeoutTime, targetOpacity: 0f);
+
+				Reset();
+				Finished?.Invoke();
+				break;
+		}
+	}
+
+	public void Stop() => StateManager.CurrentState = StateEnum.Finished;
+
+	public void Reset()
+	{
+		Lines = [];
+		LinesWidth = [];
+		Height = 0;
+		CurrentLine = "";
+		CurrentLastLineIdx = 0;
+		CurrentLastCharIdx = 0;
+		Pauses.Clear();
+		Updated?.Invoke();
+	}
+}
--- addons/Typer/TyperNode.cs
+++ addons/Typer/TyperNode.cs
@@ -11,33 +11,20 @@ public partial class TyperNode : TextureRect
 
 	[Export] public TyperResource Resource;
 
-	string Text;
-
-	int CurrentFinalCaretBlinkTimes;
-	int CurrentFinalCaretBlinkTime;
-
-	string[] Lines;
-	string CurrentLine;
-	int CurrentLastLineIdx;
-	int CurrentLastCharIdx;
-
-	float[] LinesWidth;
-	Dictionary<int, List<(int Position, int Value)>> Pauses;
-	float Height;
-
-	public enum StateEnum
-	{
-		Started,
-		Typing,
-		Pause,
-		Finished
-	}
-
-	SimpleStateManager<StateEnum> StateManager = new(StateEnum.Started);
+	// Replace the previous internal typing state with a Typer instance
+	TyperCore TyperInstance;
 
 	AudioStreamPlayer TypingSoundNode => GetNode<AudioStreamPlayer>("TypingSound");
 
-	public override void _Ready() => ((AudioStreamRandomizer)TypingSoundNode.Stream).AddStream(-1, Resource.TypingSound);
+	public override void _Ready()
+	{
+		((AudioStreamRandomizer)TypingSoundNode.Stream).AddStream(-1, Resource.TypingSound);
+
+		// create and configure Typer
+		TyperInstance = new TyperCore(Resource, this, () => TypingSoundNode.Play());
+		TyperInstance.Updated += () => QueueRedraw();
+		TyperInstance.Finished += () => EmitSignal(SignalName.Finished);
+	}
 
 	public void Reset()
 	{
@@ -46,195 +33,57 @@ public partial class TyperNode : TextureRect
 		Hide();
 	}
 
-	public void Init(string text = "")
-	{
-		Pauses = [];
-		CurrentFinalCaretBlinkTime = 0;
-		CurrentFinalCaretBlinkTimes = 0;
-		CurrentLine = "";
-		CurrentLastCharIdx = 0;
-		CurrentLastLineIdx = 0;
-
-		if (text == string.Empty)
-			text = Resource.Text;
-
-		Lines = text.Split([System.Environment.NewLine], StringSplitOptions.None);
-
-		LinesWidth = new float[Lines.Length];
-
-		for (int i = 0; i < Lines.Length; i++)
-		{
-			var size = Resource.Font.GetStringSize(Lines[i], fontSize: Resource.FontSize);
-			LinesWidth[i] = size.X;
-			Height += size.Y;
-
-			if (i < Lines.Length - 1)
-				Height += Resource.LineSpacing;
-
-			var pauses = ExtractPauses(ref Lines[i]);
-
-			if (pauses.Count > 0)
-				Pauses.Add(i, pauses);
-		}
-
-		Show();
-	}
+	public void Init(string text = "") => TyperInstance.Init(text);
 
 	public async void Start()
 	{
-		await Task.Delay((int)(Resource.StartDelay * 1000));
-		SwitchState(StateEnum.Typing);
-		await TypeLoop();
+		await TyperInstance.Start();
 	}
 
-	private async Task TypeLoop()
-	{
-		while (true)
-		{
-			if (CurrentLastLineIdx >= Lines.Length)
-			{
-				CurrentFinalCaretBlinkTimes = Resource.FinalCaretBlinkTimes;
-				SwitchState(StateEnum.Pause);
-				break;
-			}
-
-			if (StateManager.CurrentState != StateEnum.Typing)
-				break;
-
-			CurrentLine = Lines[CurrentLastLineIdx];
-
-			if (CurrentLastCharIdx < CurrentLine.Length)
-			{
-				if (Pauses.TryGetValue(CurrentLastLineIdx, out var pausePositions))
-				{
-					foreach (var (position, value) in pausePositions.ToList())
-					{
-						if (CurrentLastCharIdx == position)
-						{
-							CurrentFinalCaretBlinkTimes = value;
-							SwitchState(StateEnum.Pause);
-							pausePositions.Remove((position, value));
-							return;
-						}
-					}
-				}
-
-				CurrentLastCharIdx++;
-				QueueRedraw();
-				TypingSoundNode.Play();
-			}
-			else
-			{
-				CurrentLastLineIdx++;
-				CurrentLastCharIdx = 0;
-			}
-
-			await Task.Delay((int)(Resource.TypingSpeed * 1000));
-		}
-	}
-
-	static List<(int Position, int Value)> ExtractPauses(ref string input)
-	{
-		List<(int Position, int Value)> tags = [];
-
-		// Match tags using regular expression
-		string pattern = @"(?<!\\)\[([^\]]+)\]";
-
-		do
-		{
-			Match match = Regex.Match(input, pattern);
-
-			if (match.Success)
-			{
-				input = input.Remove(match.Index, match.Length);
-				tags.Add((match.Index, match.Groups[1].Value.ToInt()));
-			}
-			else
-				break;
-
-		} while (true);
-
-		return [.. tags.OrderBy(tag => tag.Value)];
-	}
+	public void Stop() => TyperInstance.Stop();
 
 	public override void _Draw()
 	{
-		if (StateManager.CurrentState == StateEnum.Started)
+		var state = TyperInstance.CurrentState;
+		if (state == TyperCore.StateEnum.Started)
 		{
 			base._Draw();
 			return;
 		}
 
-		if (CurrentLine != null)
+		if (TyperInstance.CurrentLine != null)
 		{
 			var pos = Vector2.Zero;
-
 			var printedLine = "";
 
-			for (int lineIdx = 0; lineIdx <= CurrentLastLineIdx; lineIdx++)
+			for (int lineIdx = 0; lineIdx <= TyperInstance.CurrentLastLineIdx; lineIdx++)
 			{
-				if (lineIdx < Lines.Length)
+				if (lineIdx < TyperInstance.Lines.Length)
 				{
-					var currentLine = Lines[lineIdx];
+					var currentLine = TyperInstance.Lines[lineIdx];
 
-					if (lineIdx < CurrentLastLineIdx)
+					if (lineIdx < TyperInstance.CurrentLastLineIdx)
 						printedLine = currentLine;
 					else
-						printedLine = currentLine[..CurrentLastCharIdx];
+						printedLine = currentLine[..TyperInstance.CurrentLastCharIdx];
 
 					printedLine = printedLine.ReplaceN(@"\\", "");
 
 					pos = new Vector2(0, Resource.FontSize + Resource.LineSpacing * lineIdx);
 
 					if (Resource.CenterHorizontally)
-						pos.X += Size.X / 2 - LinesWidth[lineIdx] / 2;
+						pos.X += Size.X / 2 - TyperInstance.LinesWidth[lineIdx] / 2;
 
 					if (Resource.CenterVertically)
-						pos.Y += Size.Y / 2 - Height / 2;
+						pos.Y += Size.Y / 2 - TyperInstance.Height / 2;
 
 					DrawString(Resource.Font, pos, printedLine, fontSize: Resource.FontSize);
 				}
 			}
 
 			// Draw caret
-			if (CurrentFinalCaretBlinkTime % 2 == 0 && Resource.Caret != "")
+			if (TyperInstance.CurrentFinalCaretBlinkTime % 2 == 0 && Resource.Caret != "")
 				DrawChar(Resource.Font, pos + new Vector2(Resource.Font.GetStringSize(printedLine, fontSize: Resource.FontSize).X, 0), Resource.Caret, fontSize: Resource.FontSize);
 		}
 	}
-
-	public async void SwitchState(StateEnum newState)
-	{
-		StateManager.CurrentState = newState;
-
-		switch (newState)
-		{
-			case StateEnum.Typing:
-				CurrentFinalCaretBlinkTime = 0;
-				await TypeLoop();
-				break;
-			case StateEnum.Pause:
-				while (CurrentFinalCaretBlinkTime < (CurrentFinalCaretBlinkTimes * 2) - 1)
-				{
-					CurrentFinalCaretBlinkTime++;
-					QueueRedraw();
-					await Task.Delay((int)(Resource.CaretBlinkTime * 1000));
-				}
-				if (CurrentLastLineIdx == Lines.Length)
-					SwitchState(StateEnum.Finished);
-				else
-					SwitchState(StateEnum.Typing);
-				break;
-			case StateEnum.Finished:
-				if (Resource.PreFadeoutTime > 0)
-					await Task.Delay((int)(Resource.PreFadeoutTime * 1000));
-
-				await FadeHelper.TweenFadeModulate(this, FadeHelper.FadeDirectionEnum.Out, Resource.FadeoutTime, targetOpacity: 0f);
-
-				Reset();
-				EmitSignal(SignalName.Finished);
-				break;
-		}
-	}
-
-	public void Stop() => StateManager.CurrentState = StateEnum.Finished;
 }
